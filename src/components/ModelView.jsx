bdd482131f7cfb580eaf4007c4953091da38ce22
Fariz <farizadzmir@gmail.com>
2025-02-01 09:04:14 +0800

    Update 0.7

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,12 +1,25 @@
-import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
+import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei";
 
 import * as THREE from 'three'
 import Lights from './Lights';
 import Loader from './Loader';
 import IPhone from './IPhone';
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  useEffect(() => {
+    if (isMobile) {
+      document.body.style.overflowX = "hidden";
+      document.documentElement.style.overflowX = "hidden";
+    }
+    return () => {
+      document.body.style.overflowX = "";
+      document.documentElement.style.overflowX = "";
+    };
+  }, []);
+
   return (
     <View
       index={index}
@@ -20,15 +33,17 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, si
 
       <Lights />
 
-      <OrbitControls 
-        makeDefault
-        ref={controlRef}
-        enableZoom={false}
-        enablePan={false}
-        rotateSpeed={0.4}
-        target={new THREE.Vector3(0, 0 ,0)}
-        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
-      /> 
+      {!isMobile && (
+        <OrbitControls 
+          makeDefault
+          ref={controlRef}
+          enableZoom={false}
+          enablePan={false}
+          rotateSpeed={0.4}
+          target={new THREE.Vector3(0, 0 ,0)}
+          onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+        />
+      )}
 
       <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0 ,0]}>
         <Suspense fallback={<Loader />}>
